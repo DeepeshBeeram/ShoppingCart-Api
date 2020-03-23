3f82689eb078b5ee3d2a669875e405aaa97e4bfb
DeepeshBeeram <deepesh.kp62@gmail.com>
2020-03-23 14:08:27 -0400

    Add project files.

---
+++ LinkedInProject/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinkedInProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LinkedInProject.Helpers;
+
+namespace LinkedInProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureDeleted();
+
+            _context.Database.EnsureCreated();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters )
+        {
+            IQueryable<Product> products = _context.Products;
+
+            if(queryParameters.MaxPrice != null && queryParameters.MinPrice != null)
+            {
+                products = products.Where(p => p.Price >= queryParameters.MinPrice.Value
+                && p.Price <= queryParameters.MaxPrice.Value);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParameters.Sku);
+            }
+
+            products = products.
+                Skip(queryParameters.Size * (queryParameters.Page - 1)).
+                Take(queryParameters.Size);
+
+            var allProducts = await products.ToArrayAsync();
+            return Ok(allProducts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct( int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
